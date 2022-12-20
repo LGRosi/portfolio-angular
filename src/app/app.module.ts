18fd76d4220a6b9d651532a62d382d10a74ad11d
LGRosi <lrosi1996@gmail.com>
2022-12-20 18:23:26 -0300

    Migracion de css a scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SdfComponent } from './sdf/sdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SdfComponent
   ],
   imports: [
     BrowserModule
