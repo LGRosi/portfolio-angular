86c71a14b5ca589f1b46ae5431d76c30a76cca9c
LGRosi <lrosi1996@gmail.com>
2022-12-20 21:30:58 -0300

    Creacion de la estructura de carpetas para el proyecto

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+// General Modules
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class PagesModule {}
